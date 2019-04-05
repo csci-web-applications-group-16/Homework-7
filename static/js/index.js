cd39c129c58506695e0c712576654a87f7cbaab7
Navid Kabir <navid113@gmail.com>
2019-04-04 21:49:19 -0400

    Remove templates, replace with regular pages, I'm not crazy

--- static/js/index.js
+++ static/js/index.js
@@ -1,65 +1,3 @@
-const PAGES = {
-    'UPLOAD': {
-        'url': 'fragments/upload.htm'
-    },
-    'VERIFY': {
-        'url': 'fragments/verify.htm'
-    },
-    'CONFIGURE': {
-        'url': 'fragments/configure.htm'
-    },
-    'RUN TESTS': {
-        'url': 'fragments/tests.htm'
-    },
-    'REVIEW': {
-        'url': 'fragments/review.htm'
-    },
-};
-
-let currentPageIndex = 0;
-
-function renderBreadcrumbs() {
-    let output = "<ol class='breadcrumb'>";
-    let strings = Object.keys(PAGES);
-
-    for (let i = 0; i < strings.length; i++) {
-        if (i === currentPageIndex) {
-            output += `<li class='breadcrumb-item active' aria-current='page'>${strings[i]}</li>`;
-        } else {
-            output += `<li class='breadcrumb-item'>${strings[i]}</li>`;
-        }
-    }
-
-    output += '</ol>';
-
-    $('#breadcrumbs').html(output);
-}
-
-function loadPage() {
-    const key = Object.keys(PAGES)[currentPageIndex];
-    $('#page-content').load(PAGES[key].url);
-}
-
-function previousPage() {
-    currentPageIndex--;
-    if (currentPageIndex < 0) currentPageIndex = 0;
-
-    refresh();
-}
-
-function nextPage() {
-    currentPageIndex++;
-    const length = Object.keys(PAGES).length;
-    if (currentPageIndex >= length - 1) currentPageIndex = length - 1;
-
-    refresh();
-}
-
-function refresh() {
-    renderBreadcrumbs();
-    loadPage();
-}
-
 function setCookie(name, val) {
     document.cookie = `${name}=${val}`;
 }
@@ -72,7 +10,3 @@ function getCookie(name) {
     const v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
     return v ? v[2] : null;
 }
-
-window.onload = function () {
-    refresh();
-};
